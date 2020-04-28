c0a9cc0dadb67f166a3dc05a15a7077e73a8b916
MoonJiao123 <yuj001@ucsd.edu>
2020-04-28 12:59:49 -0700

    model/controller for business set up can add/delete/search business

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.sequelize.sync();
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to FUO." });
+});
+
+require("./routes/business.routes")(app);
+
+const port = process.env.PORT || 5000;
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
