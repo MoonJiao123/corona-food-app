f42a3c15e788229202f5b3fe8c15bad4688c118f
Thuyet Ta <thuyetta.tom@gmail.com>
2020-05-03 15:21:48 -0700

    updated left sidebar component

--- client/src/components/LeftSideBar.js
+++ client/src/components/LeftSideBar.js
@@ -7,34 +7,48 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from "@material-ui/core/Button";
 
-const Style = styled.div`
+const LeftSidebarDiv = styled.div`
     position: fixed; /* stay in place on scroll and position relative to view */
     left: 0;
     height: 100%;
     width: 400 px;
     z-index: 1;  /* stay on top of everything */
     color: white;
-    background-color: black;
+    background-color: #67d367;
     overflow-x: hidden; /* disable horizontal scroll */
     padding-top: 10px; 
     display: inline-block;
+    overflow:auto;
+    max-width: 30%; /* stops div from resizing */
+    min-width: 30%;
 `;
 
-const divStyle = styled.div`
+const ButtonsDiv = styled.div`
+   text-align: center;
+   padding-bottom: 20px;
    position: absolute;
    bottom: 10px;
    width: 100%;
 `;
 
+const CompanyName = styled.h2`
+    text-align: center;
+    margin-bottom: 30px;
+`
+const CompanyMission = styled.p`
+    margin-left: 5%;
+    font-size: 17px;
+    margin-bottom: 30px;
+`
 
 class SideNav extends React.Component {
     render() {
         return(
-            <Style>
-             <img src={"https://via.placeholder.com/300.jpg"} width={200}/>
-             <h2> Company Name </h2>
-             <h3> Mission</h3>
-             <divStyle>
+            <LeftSidebarDiv>
+             <img src={"media/mascot.jpg"} width={200}/>
+              <CompanyName> Company Name </CompanyName>
+             <CompanyMission> This is where company would insert their mission statement. </CompanyMission>
+             <ButtonsDiv>
              <Button
                 type="submit"
                 size={"small"}
@@ -50,8 +64,8 @@ class SideNav extends React.Component {
                  color="default  ">
                   Add Location
              </Button>
-              </divStyle>
-            </Style>
+             </ButtonsDiv>
+            </LeftSidebarDiv>
         );
     }
 }
