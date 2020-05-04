0bb1e0b171fc316bd717326f852f67ecd98ebd07
Darien Tsai <smartdragondt@gmail.com>
2020-05-04 09:29:05 -0700

    More refactoring, updated package-lock

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.css';
 import App from './App';
 import ErrorPage from './components/ErrorPage';
 import BusinessDashboardParent from './components/BusinessDashboardParent';
