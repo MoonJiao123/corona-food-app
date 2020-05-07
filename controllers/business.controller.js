48b9df79f7d502241d3a1a8144c73c0c880dc98b
MoonJiao123 <yuj001@ucsd.edu>
2020-05-07 16:58:36 -0700

    not quite working be

--- controllers/business.controller.js
+++ controllers/business.controller.js
@@ -1,41 +1,58 @@
-const db = require("../models");
+const db = require("../config/db.config");
 const Business = db.business;
 const Op = db.Sequelize.Op;
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
+const Business = db.business;
 
-// Create and Save a new business_account
-exports.create = (req, res) => {
-   // Validate request
-   if (!req.body.account) {
-    res.status(400).send({
-      message: "No accounts!"
-    });
-    console.log('Got body:', req.body);
-    return;
-  }
-
-  // Create a Business
-  const business = {
-    business_id: req.body.business_id,
-    account: req.body.account,
-    password: req.body.password,
+exports.signup = (req, res) => {
+  // Save Business to Database
+  console.log("Processing func -> SignUp");
+  
+  Business.create({
+    name: req.body.name,
     email: req.body.email,
-    mobile: req.body.mobile
-  };
-
-  // Save Business in the database
-  Business.create(business)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Business_account."
-      });
-    });
+    account: req.body.account,
+    mobile: req.body.mobile,
+    password: bcrypt.hashSync(req.body.password, 8)
+  }).then(user => {
+    res.send("User registered successfully!");
+  }).catch(err => {
+    res.status(500).send("Fail! Error -> " + err);
+  })
 }
+// // Create and Save a new business_account
+// exports.create = (req, res) => {
+//    // Validate request
+//    if (!req.body.account) {
+//     res.status(400).send({
+//       message: "No accounts!"
+//     });
+//     console.log('Got body:', req.body);
+//     return;
+//   }
+
+//   // Create a Business
+//   const business = {
+//     business_id: req.body.business_id,
+//     account: req.body.account,
+//     password: req.body.password,
+//     email: req.body.email,
+//     mobile: req.body.mobile
+//   };
+
+//   // Save Business in the database
+//   Business.create(business)
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while creating the Business_account."
+//       });
+//     });
+// }
 
 // Retrieve all businesss_account from the database.
 exports.findAll = (req, res) => {
@@ -54,85 +71,112 @@ exports.findAll = (req, res) => {
       });
 };
 
-// Find a single business_account with an id
-exports.findOne = (req, res) => {
-    const id = req.params.business_id;
+exports.signin = (req, res) => {
+  console.log("Sign-In");
+  
+  Business.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then(user => {
+    if (!user) {
+      return res.status(404).send('User Not Found.');
+    }
+ 
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({ auth: false, accessToken: null, reason: "Invalid Password!" });
+    }
+    
+    var token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+    
+    res.status(200).send({ auth: true, accessToken: token });
+    
+  }).catch(err => {
+    res.status(500).send('Error -> ' + err);
+  });
+}
+// // Find a single business_account with an id
+// exports.findOne = (req, res) => {
+//     const id = req.params.business_id;
 
-    Business.findByPk(id)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error retrieving business account with id=" + id
-        });
-      });
-};
+//     Business.findByPk(id)
+//       .then(data => {
+//         res.send(data);
+//       })
+//       .catch(err => {
+//         res.status(500).send({
+//           message: "Error retrieving business account with id=" + id
+//         });
+//       });
+// };
 
-// Update a business_account by the id in the request
-exports.update = (req, res) => {
-    const id = req.params.business_id;
+// // Update a business_account by the id in the request
+// exports.update = (req, res) => {
+//     const id = req.params.business_id;
 
-    Business.update(req.body, {
-      where: { business_id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "Account was updated successfully."
-          });
-        } else {
-          res.send({
-            message: `Cannot update Account with id=${id}. Maybe Business was not found or req.body is empty!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error updating Busienss with id=" + id
-        });
-      });
-};
+//     Business.update(req.body, {
+//       where: { business_id: id }
+//     })
+//       .then(num => {
+//         if (num == 1) {
+//           res.send({
+//             message: "Account was updated successfully."
+//           });
+//         } else {
+//           res.send({
+//             message: `Cannot update Account with id=${id}. Maybe Business was not found or req.body is empty!`
+//           });
+//         }
+//       })
+//       .catch(err => {
+//         res.status(500).send({
+//           message: "Error updating Busienss with id=" + id
+//         });
+//       });
+// };
 
-// Delete a business_account with the specified id in the request
-exports.delete = (req, res) => {
-    const id = req.params.business_id;
+// // Delete a business_account with the specified id in the request
+// exports.delete = (req, res) => {
+//     const id = req.params.business_id;
 
-    Business.destroy({
-      where: { business_id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "business account was deleted successfully!"
-          });
-        } else {
-          res.send({
-            message: `Cannot delete business account with id=${id}. Maybe business account was not found!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Could not delete business account with id=" + id
-        });
-      });
-};
+//     Business.destroy({
+//       where: { business_id: id }
+//     })
+//       .then(num => {
+//         if (num == 1) {
+//           res.send({
+//             message: "business account was deleted successfully!"
+//           });
+//         } else {
+//           res.send({
+//             message: `Cannot delete business account with id=${id}. Maybe business account was not found!`
+//           });
+//         }
+//       })
+//       .catch(err => {
+//         res.status(500).send({
+//           message: "Could not delete business account with id=" + id
+//         });
+//       });
+// };
 
-// Delete all businesss_account from the database.
-exports.deleteAll = (req, res) => {
-    Business.destroy({
-        where: {},
-        truncate: false
-      })
-        .then(nums => {
-          res.send({ message: `${nums} business_accounts were deleted successfully!` });
-        })
-        .catch(err => {
-          res.status(500).send({
-            message:
-              err.message || "Some error occurred while removing all business accounts."
-          });
-        });
-};
+// // Delete all businesss_account from the database.
+// exports.deleteAll = (req, res) => {
+//     Business.destroy({
+//         where: {},
+//         truncate: false
+//       })
+//         .then(nums => {
+//           res.send({ message: `${nums} business_accounts were deleted successfully!` });
+//         })
+//         .catch(err => {
+//           res.status(500).send({
+//             message:
+//               err.message || "Some error occurred while removing all business accounts."
+//           });
+//         });
+// };
 
