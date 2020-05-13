799f34c7fca28c0aa8b01264a2f7c251faa94d00
Darien_Tsai <30358377+DarienTsai@users.noreply.github.com>
2020-05-13 15:54:43 -0700

    Overriding someone overriding my code...
    why

--- client/src/components/updateListings.js
+++ client/src/components/updateListings.js
@@ -10,6 +10,7 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
+import 'date-fns';
 
 /* ---------------------------------------------------------------------
 An individual, editable listing
@@ -19,46 +20,72 @@ class ListingForm extends React.Component{
   constructor(props){
     super(props);
 
+    //state design
     this.state = {
       category: "none",
+      fileInput: <input type="file" accept=".jpg,.jpeg,.png" className="updateFile" ref={input => this.imgFile = input}/>
     }
+
+    //bindings
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleImg = this.handleImg.bind(this);
   }
 
   //event handlers
-  handleChange = (event) => {
+  handleSelect = (event) => {
     this.setState({category: event.target.value});
   };
 
+  handleImg = (e) => {
+    e.preventDefault();
+    this.imgFile.click();
+  }
+
   render(){
-    <div className="updatableListing">
-      <input type="file" accept=".jpg,.jpeg,.png">Image</input>
+    
+    return(
+      <form className="updatableListing">
+        <button className="updateFileWrap" onClick={this.handleImg}>Image</button>
+        {this.state.fileInput}
 
-      <FormControl variant="filled">
-        <InputLabel id="category select">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-filled-label"
-          id="demo-simple-select-filled"
-          value="Category"
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Vegetable</MenuItem>
-          <MenuItem value={20}>Meat</MenuItem>
-          <MenuItem value={30}>Grain</MenuItem>
-          <MenuItem value={40}>Fruit</MenuItem>
-          <MenuItem value={50}>Dairy</MenuItem>
-          <MenuItem value={60}>Snack</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl>
+          <InputLabel className="category-select">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-filled-label"
+            className="updateSelect"
+            value={this.state.category}
+            onChange={this.handleSelect}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Vegetable</MenuItem>
+            <MenuItem value={20}>Meat</MenuItem>
+            <MenuItem value={30}>Grain</MenuItem>
+            <MenuItem value={40}>Fruit</MenuItem>
+            <MenuItem value={50}>Dairy</MenuItem>
+            <MenuItem value={60}>Snack</MenuItem>
+          </Select>
+        </FormControl>
 
-      <TextField id="filled-basic" label="Name" variant="filled" />
-      <TextField id="filled-basic" label="Amount" variant="filled" />
-      <TextField id="filled-basic" label="Price" variant="filled" />
-      <TextField id="filled-basic" label="Expiration" variant="filled" />
-      <TextField id="filled-basic" label="min - max % off" variant="filled" />
-    </div>
+        <TextField className="updateField" label="Name" />
+        <TextField className="updateField" label="Amount" />
+        <TextField className="updateField" label="Price" />
+        <TextField className="updateField" label="min - max%" />
+
+        <TextField
+          id="date"
+          label="Expiration"
+          type="date"
+          className="upDate"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+
+        <button className="removeListing">-</button>
+      </form>
+    );
   }
 }
 
@@ -74,18 +101,22 @@ class UpdateListings extends React.Component{
 
     //return popup form
     return(
-      <div id="updateListings" className={this.props.toggle}>
+      <div id="updateWrapper" className={this.props.toggle}>
+        <div id="updateListings">
 
-        <div id="updateList">
-          <ListingForm/>
-        </div>
-        
-        <div id="updateControls">
-          <button>+</button>
-          <Button variant="contained">Default</Button>
-          <Button variant="contained">Default</Button>
-        </div>
-      </div>  
+          <div id="updateList">
+            <ListingForm/>
+            <ListingForm/>
+            <ListingForm/>
+          </div>
+
+          <div id="updateControls">
+            <button id="addListing">+</button>
+            <Button variant="contained" onClick={this.props.data.submitUpdate}>Save</Button>
+            <Button variant="contained" id="cancelUpdate" onClick={this.props.data.closeForm}>Cancel</Button>
+          </div>
+        </div>  
+      </div>
     );
   }
 }
