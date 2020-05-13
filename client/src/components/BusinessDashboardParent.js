9a41b1ccf612084c20f0484776b6bdcd10a2b91e
Darien_Tsai <30358377+DarienTsai@users.noreply.github.com>
2020-05-13 15:53:44 -0700

    Overriding someone overriding my code...
    why

--- client/src/components/BusinessDashboardParent.js
+++ client/src/components/BusinessDashboardParent.js
@@ -9,6 +9,7 @@ import LeftSideBar from './LeftSideBar';
 import LocationSearchBar from "./LocationSearchBar";
 import Locations from "./Locations";
 import AddLocation from './AddLocation';
+import UpdateListings from './UpdateListings';
 /* ---------------------------------------------------------------------
 Component Object: will contain the left/center/right components that make
 up the business dashboard.
@@ -58,7 +59,9 @@ class BusinessDashboardParent extends React.Component{
             expiration: ''
           }
         ],
-        updateProducts: () => alert("update products")
+        updateProducts: () => {
+          this.setState({updateClass: this.state.updateClass==="off"?"on":"off"});
+        }
       },
 
       formClass: "off",
@@ -68,6 +71,16 @@ class BusinessDashboardParent extends React.Component{
         closeForm: (e) => {
           e.preventDefault();
           this.setState({formClass: this.state.formClass==="off"?"on":"off"});
+        },
+      },
+
+      updateClass: "off",
+      
+      update: {
+        submitUpdate: () => alert("listings updated"),
+        closeForm: (e) => {
+          e.preventDefault();
+          this.setState({updateClass: this.state.updateClass==="off"?"on":"off"});
         },
       }
 
@@ -101,7 +114,9 @@ class BusinessDashboardParent extends React.Component{
             expiration: '11/11/2020'
           }
         ],
-        updateProducts: () => alert("update products")
+        updateProducts: () => {
+          this.setState({updateClass: this.state.updateClass==="off"?"on":"off"});
+        }
       }
     });
 
@@ -150,6 +165,7 @@ class BusinessDashboardParent extends React.Component{
         <Locations data = {this.state.center}/>
         <LocationInfo data={this.state.right}/>
         <AddLocation data={this.state.form} toggle={this.state.formClass}/>
+        <UpdateListings data={this.state.update} toggle={this.state.updateClass}/>
       </div>
     );
   }
