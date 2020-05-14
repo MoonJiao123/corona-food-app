d3b1a257694702207893309f774f2926aa56370f
mrtiggles5 <dta858king@gmail.com>
2020-05-14 12:45:15 -0700

    Added function header for generate API key as I've been planning out its implementation

---
+++ controllers/BusinessAPIController.js
@@ -0,0 +1,41 @@
+
+
+ /**
+ * This file sets up the API endpoint to which the API key is generated
+ * Contributors: 
+ */
+
+const express = require('express')
+const businessUsers = express.Router()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const BUser = require('../models/BusinessModel.js')
+businessUsers.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+
+//PROFILE
+//to fetch profile from FE.
+businessUsers.get('/business', (req, res) => {
+  
+    BUser.findOne({
+      where: {
+        id: decoded.id
+      }
+    })
+      .then(user => {
+        if (user) {
+          res.json(user)
+        } else {
+          res.send('User does not exist')
+        }
+      })
+      .catch(err => {
+        res.send('error: ' + err)
+      })
+  })
+  
+  module.exports = businessUsers
+
